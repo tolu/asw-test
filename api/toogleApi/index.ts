40bbce7a187a637cbed2b6ecf30592796448134e
Tobias Lundin <tobias.lundin@gmail.com>
2020-05-26 10:02:52 +0200

    hmm

--- api/toogleApi/index.ts
+++ api/toogleApi/index.ts
@@ -2,11 +2,15 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
+    const header = req.headers["x-ms-client-principal"];
+    const encoded = Buffer.from(header, "base64");
+    const decoded = encoded.toString("ascii");
+    const user = JSON.parse(decoded);
     const name = (req.query.name || (req.body && req.body.name)) ?? 'Fallback Name';
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: "Hello " + name
+        body: `Hello ${name} (${user?.userDetails ?? 'anonymous'})`
     };
 };
 
