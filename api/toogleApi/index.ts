f4892e727bacd3136977c15ab08b91402a8d96c4
Tobias Lundin <tobias.lundin@gmail.com>
2020-05-26 10:10:28 +0200

    try catch get user in function

--- api/toogleApi/index.ts
+++ api/toogleApi/index.ts
@@ -2,10 +2,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const header = req.headers["x-ms-client-principal"];
-    const encoded = Buffer.from(header, "base64");
-    const decoded = encoded.toString("ascii");
-    const user = JSON.parse(decoded);
+    let user = getUser(req);
     const name = (req.query.name || (req.body && req.body.name)) ?? 'Fallback Name';
 
     context.res = {
@@ -15,3 +12,14 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 };
 
 export default httpTrigger;
+
+const getUser = (req: HttpRequest) => {
+    try {
+        const header = req.headers["x-ms-client-principal"];
+        const encoded = Buffer.from(header, "base64");
+        const decoded = encoded.toString("ascii");
+        return JSON.parse(decoded);
+    } catch (e) {
+        return null;
+    }
+}
